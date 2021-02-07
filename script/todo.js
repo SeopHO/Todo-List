6929281ce273aa28cfcb73506716d726f09aa9d4
SeopHO <56343122+SeopHO@users.noreply.github.com>
2021-02-07 13:03:29 +0900

    Add files via upload

--- script/todo.js
+++ script/todo.js
@@ -5,12 +5,15 @@ const todolist = document.querySelector(".todolist");
 
 let arr = [];
 
+let totalNum;
+let CountNum;
 
 function drawtodolist(text)
 {
     const list_Single = document.createElement("div");
     const menu_Single = document.createElement("div");
     const menu_Single_1 = document.createElement("div");
+
     list_Single.className = "list-single";
     menu_Single.className = "menu-single";
     menu_Single_1.className = "menu-single-1";
@@ -18,18 +21,21 @@ function drawtodolist(text)
     const span = document.createElement("span");
     span.innerHTML = text;
     li.appendChild(span);
+
     list_Single.appendChild(li);
     menu_Single.appendChild(menu_Single_1);
     list_Single.appendChild(menu_Single);
     menu_Single_1.innerHTML = `<i class="deleteSingle fa fa-trash-o fa-lg" style="color:gray; cursor: pointer;" aria-hidden="true"></i>`;
     todolist.appendChild(list_Single);
 
+    
     li.addEventListener("click",function()
     {
-        console.log('click!');
-
+        // span.innerHTML = `<hr width="100%" align="center">`;
+        // const line = document.createElement("hr");
+        // li.appendChild(line);
+        li.classList.toggle("single-clear");
     });
-    
 }
 
 function handleSubmit(event)
