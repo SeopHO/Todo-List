2779f49d395cc82e647180ba84d053dadafe7c20
SeopHO <56343122+SeopHO@users.noreply.github.com>
2021-02-09 23:22:21 +0900

    Add files via upload

--- script/todo.js
+++ script/todo.js
@@ -1,72 +1,12 @@
+//Selector
 const todoform = document.querySelector(".todo-form"),
 todoInput = todoform.querySelector(".todo");
-
 const todolist = document.querySelector(".todolist");
 
-let arr = [];
+//Event Listener
+todoform.addEventListener("submit",handleSubmit);
 
-function totalNulCalcul(num)
-{
-    const CntTotal = document.querySelector(".total");
-    CntTotal.innerHTML = num;
-}
-function countNumCalcul(num)
-{
-    const CntNum = document.querySelector(".number");
-    CntNum.innerHTML = num;
-}
-function drawtodolist(text)
-{
-    const div_list_Single = document.createElement("div");
-    const div_menu_Single = document.createElement("div");
-    const div_menu_Single_box1 = document.createElement("div");
-
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-
-    div_list_Single.className = "list-single";
-    div_menu_Single.className = "menu-single";
-    div_menu_Single_box1.className = "menu-single-box1";
-
-    span.innerHTML = text;
-    div_menu_Single_box1.innerHTML = `<i class="delete-Single fa fa-trash-o fa-lg" style="color:gray; cursor: pointer;" aria-hidden="true"></i>`;
-
-    li.appendChild(span);
-    div_list_Single.appendChild(li); //final
-
-    div_menu_Single.appendChild(div_menu_Single_box1);
-
-    div_list_Single.appendChild(div_menu_Single); //final
-    todolist.appendChild(div_list_Single);
-    
-    const listSingles = document.querySelectorAll("div .list-single");
-    const deleteSingle = document.querySelector("div .delete-Single");
-    
-    li.addEventListener("click",function()
-    {
-        li.classList.toggle("single-clear");
-        const singleClear = document.querySelectorAll(".single-clear");
-        countNumCalcul(singleClear.length);
-    });
-
-    // listSingles.forEach(function(single)
-    // {
-    //     single.addEventListener("click",function(event)
-    //     {
-    //         const bigParent = single.closest(".list-single");
-    //         bigParent.remove();
-    //         console.log(listSingles.length);
-    //         totalNulCalcul(listSingles.length);
-    //     })
-    // });
-    // totalNulCalcul(listSingles.length);
-
-    deleteSingle.addEventListener("click",function()
-    {
-        const bigParent = deleteSingle.closest(".list-single"); 
-        bigParent.remove();
-    });
-}
+//function
 function handleSubmit(event)
 {
     event.preventDefault();
@@ -75,8 +15,56 @@ function handleSubmit(event)
     drawtodolist(value);
     // console.log(value);
 }
-function init()
+
+function drawtodolist(text)
 {
-    todoform.addEventListener("submit",handleSubmit);
+    //create Element
+    const div_list_Single = document.createElement("div");
+    div_list_Single.className = "list-single";
+
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerHTML = text;
+
+    const div_menu_Single = document.createElement("div");
+    div_menu_Single.className = "menu-single";
+    const div_menu_Single_box1 = document.createElement("div");
+    div_menu_Single_box1.className = "menu-single-box1";
+    const trashBtn = document.createElement("i");
+    trashBtn.classList.add("trashBtn");
+    trashBtn.classList.add("fa");
+    trashBtn.classList.add("fa-trash-o");
+    trashBtn.classList.add("fa-lg");
+
+    li.appendChild(span);
+    div_list_Single.appendChild(li);
+
+    div_menu_Single_box1.appendChild(trashBtn);
+    div_menu_Single.appendChild(div_menu_Single_box1);
+    div_list_Single.appendChild(div_menu_Single);
+
+    todolist.appendChild(div_list_Single); //final
+
+    //animation
+    li.addEventListener("click",function()
+    {
+        li.classList.toggle("single-clear");
+    })
+
+    //trashBtn
+    trashBtn.addEventListener('click',function()
+    {
+        const bigParent = trashBtn.closest(".list-single");
+        bigParent.remove();
+
+    });
 }
-init();
+
+function todoTotalCount(num)
+{
+       
+}
+function todoNumCount(num)
+{
+
+}
