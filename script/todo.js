28f30040994c58070e3b4d0ee55464a9b11e6c8b
SeopHO <56343122+SeopHO@users.noreply.github.com>
2021-02-11 23:49:43 +0900

    Add files via upload

--- script/todo.js
+++ script/todo.js
@@ -38,12 +38,7 @@ function loadList()
     const loadLIST = localStorage.getItem(LIST_LS);
     if(loadLIST !== null)
     {
-        // const parsesList = JSON.parse(loadLIST);
-        console.log(loadLIST);
-        for(let i=0;i<loadLIST.length;i++)
-        {
-            drawtodolist(loadLIST[i]);
-        }
+
     }
 }
 function drawtodolist(text)
@@ -53,6 +48,7 @@ function drawtodolist(text)
     div_list_Single.className = "list-single";
 
     const li = document.createElement("li");
+    li.id = listArray.length;
     const span = document.createElement("span");
     span.innerHTML = text;
 
@@ -74,19 +70,29 @@ function drawtodolist(text)
     div_menu_Single.appendChild(div_menu_Single_box1);
     div_list_Single.appendChild(div_menu_Single);
 
+    const listObj={
+        id:listArray.length,
+        text:text,
+        finish:false,
+    }
+    listArray.push(listObj);
+    console.log(listArray);
+    
+
     todolist.appendChild(div_list_Single); //final
 
     //animation
-    li.addEventListener("click",function()
+    li.addEventListener("click",function(single)
     {
         li.classList.toggle("single-clear");
+        const liId = li.id;
         if(li.classList.contains("single-clear"))
         {
-            todoClearCount(text,"increase");
+            listArray[liId].finish=true;
         }
         else if(!(li.classList.contains("single-clear")))
         {
-            todoClearCount(text,"decrease");
+            listArray[liId].finish=false;
         }
     })
 
@@ -95,33 +101,32 @@ function drawtodolist(text)
     {   
         const bigParent = trashBtn.closest(".list-single");
         bigParent.remove();
-        todoTotalCount(span.textContent,"decrease");
-        todoClearCount(span.textContent,"decrease");
-    });
-    todoTotalCount(text,"increase");
-    saveList();
-}
-function todoTotalCount(value,check)
-{
-    const countingContentTotal = document.querySelector(".total");
-    if(check === "increase")
-    {
-        listArray.push(`${value}`);
-        // console.log(listArray);
-    }
-    else if(check === "decrease")
-    {
+        const liId = li.id;
+        console.log('id',liId);
+        todoTotalCount(listObj,liId,"decrease");
         for(let i=0;i<listArray.length;i++)
         {
-            if(listArray[i] === `${value}`)
-            {
-                const idx = listArray.indexOf(`${value}`);
-                // console.log('find it!',value,idx);
-                if(idx>-1)
-                    listArray.splice(idx,1);
-            }
+            listArray[i].id = i;
+
         }
-        // console.log(listArray);
+        const liAll = document.querySelectorAll("li");
+        let numId=0;
+        liAll.forEach(function(single_li)
+        {
+            single_li.id = numId++;
+        });
+        // todoClearCount(span.textContent,"decrease");
+    });
+    todoTotalCount();
+    // saveList();
+}
+function todoTotalCount(obj=null,num=0,check="increase")
+{
+    const countingContentTotal = document.querySelector(".total");
+    if(check === "decrease")
+    {
+        listArray.splice(num,1);
+        console.log(listArray);
     }
     countingContentTotal.innerHTML = listArray.length;
     createScrollbar();
